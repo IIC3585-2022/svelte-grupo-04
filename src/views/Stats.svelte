3b7f41f84d715f232faccc1b8e6b5f07125fcff4
FloValdes <ftvaldes1@uc.cl>
2022-06-04 17:08:51 -0400

    feat(stats): add stats and make text as html

---
+++ src/views/Stats.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { store } from '../store/store';
+</script>
+
+<main>
+  <h1 class="mb-10 text-3xl font-bold underline bg-green-500">Stats</h1>
+  <div class="flex flex-col w-1/2 mx-auto bg-white rounded-xl p-7 drop-shadow-md">
+    <div class="flex m-8 text-left">
+      <img src="../assets/controller.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+      <p>
+        Total Juegos Jugados: {$store.gamesPlayed}
+      </p>
+    </div>
+    <div class="flex m-8 text-left ">
+      <img src="../assets/check.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+      <p>
+        Total Preguntas Correctas: {$store.correctAnswersCount}
+      </p>
+    </div>
+    <div class="flex m-8 text-left">
+      <img src="../assets/questions.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+      <p>
+        Total Preguntas Respondidas: {$store.totalQuestionsAnswered}
+      </p>
+    </div>
+    <div class="flex m-8 text-left">
+      <img src="../assets/crown-2.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+      <p>
+        Mayor Cantidad de Aciertos: {$store.maxCorrectAnswers}
+      </p>
+    </div>
+  </div>
+</main>
