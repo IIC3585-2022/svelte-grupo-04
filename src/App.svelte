47900282eed829c9cb99e94db2f35816314dee52
FelipeCampbell <fcampbell@uc.cl>
2022-06-04 10:29:19 -0400

    feat(trivia): add views and logic to advance between questions and check right answers

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,62 @@
 <script>
-  export let name = 'const';
+  import { store, questions, currentIndex } from './store/store';
+  import triviaApi from './api/trivia';
+  import TriviaCard from './components/TriviaCard.svelte';
+  import TriviaQuestion from './components/TriviaQuestion.svelte';
+
+  const AMOUNT_QUESTIONS = 10;
+  // game state
+  // let questions = [];
+
+  let difficulty = null;
+  const startGame = async (diff) => {
+    await fetchNewQuestions();
+    $store.isPlaying = true;
+  };
+  const fetchNewQuestions = async (diff) => {
+    // console.log(difficulty);
+    // console.log('hola');
+    const res = await triviaApi.getQuestions({
+      amount: AMOUNT_QUESTIONS,
+      difficulty: difficulty,
+      type: 'multiple',
+    });
+    $questions = res.data.results;
+    console.log($questions[0]);
+    // console.log('Hola', veamos[0]);
+  };
 </script>
 
 <main>
-  <h1 class="bg-black px-4">Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="flex flex-col justify-center text-center">
+    <div>
+      <h1 class="mb-10 text-3xl font-bold underline">Trivia</h1>
+
+      {#if $store.isPlaying}
+        <div>
+          <p class="text-xl">
+            Respuestas Correctas
+            <span class="font-bold">{$store.correctAnswersCount} / {AMOUNT_QUESTIONS} </span>
+          </p>
+          <TriviaQuestion />
+          <p class="text-2xl">
+            {$currentIndex < 10 ? $currentIndex + 1 : AMOUNT_QUESTIONS} / {AMOUNT_QUESTIONS}
+          </p>
+        </div>
+      {:else}
+        <TriviaCard>
+          <div>
+            <p>Elige la dificultad</p>
+            <select bind:value={difficulty} class="mb-10">
+              <option disabled value="">Dificultad</option>
+              <option value="easy">Fácil</option>
+              <option value="medium">Mediana</option>
+              <option value="hard">Difícil</option>
+            </select>
+          </div>
+          <button on:click={startGame} class="rounded-lg bg-green-400 p-3 ">Empezar juego</button>
+        </TriviaCard>
+      {/if}
+    </div>
+  </div>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
