cceb32a9ed2cf1112ad2767f385a3941af1ebc30
simontoya <simontoya@uc.cl>
2022-06-05 20:18:53 -0400

    feat(addEndGame): add option to end game, fix current errors and maintain stats

--- src/components/TriviaQuestion.svelte
+++ src/components/TriviaQuestion.svelte
@@ -2,6 +2,7 @@
   import TriviaCard from './TriviaCard.svelte';
   import { store, currentIndex, currentQuestion, currentAnswers } from '../store/store';
 
+  const AMOUNT_QUESTIONS = 10;
   let answered = false;
   let correctAnswer = false;
   const sendAnswer = (correct) => {
@@ -9,13 +10,21 @@
     correctAnswer = correct;
     if (correct) {
       $store.correctAnswersCount += 1;
+      $store.totalQuestionsAnsweredRight += 1;
     }
   };
   const nextQuestion = () => {
-    $currentIndex += 1;
-    $store.totalQuestionsAnswered += 1;
-    answered = false;
-    correctAnswer = false;
+    if ($currentIndex < AMOUNT_QUESTIONS - 1) {
+      $currentIndex += 1;
+      $store.totalQuestionsAnswered += 1;
+      answered = false;
+      correctAnswer = false;
+    } else {
+      $store.totalQuestionsAnswered += 1;
+      $store.isPlaying = false;
+      $store.isGameOver = true;
+      $store.gamesPlayed += 1;
+    }
   };
 </script>
 
@@ -26,19 +35,19 @@
   {#if answered}
     <div>
       {#if correctAnswer}
-        <img src="../assets/check.svg" alt="Correct" class="w-1/3 mx-auto" />
+        <img src="../assets/check.svg" alt="Correct" class="mx-auto w-1/3" />
       {:else}
-        <img src="../assets/cross.svg" alt="Incorrect" class="w-1/3 mx-auto" />
+        <img src="../assets/cross.svg" alt="Incorrect" class="mx-auto w-1/3" />
       {/if}
       <p class="text-lg">Respuesta correcta:</p>
       <p class="mb-4 text-xl">{@html $currentQuestion.correctAnswer}</p>
-      <button class="p-3 bg-green-400 rounded-lg" on:click={() => nextQuestion()}>Siguiente</button>
+      <button class="rounded-lg bg-green-400 p-3" on:click={() => nextQuestion()}>Siguiente</button>
     </div>
   {:else}
     <div>
       {#each $currentAnswers as answer}
         <button
-          class="w-5/6 p-4 my-3 text-xl bg-green-400"
+          class="my-3 w-5/6 bg-green-400 p-4 text-xl"
           on:click={() => sendAnswer(answer.correct)}
         >
           {@html answer.value}
--- src/views/Stats.svelte
+++ src/views/Stats.svelte
@@ -3,28 +3,28 @@
 </script>
 
 <main>
-  <h1 class="mb-10 text-3xl font-bold underline bg-green-500">Stats</h1>
-  <div class="flex flex-col w-1/2 mx-auto bg-white rounded-xl p-7 drop-shadow-md">
-    <div class="flex m-8 text-left">
-      <img src="../assets/controller.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+  <h1 class="mb-10 bg-green-500 text-3xl font-bold underline">Stats</h1>
+  <div class="mx-auto flex w-1/2 flex-col rounded-xl bg-white p-7 drop-shadow-md">
+    <div class="m-8 flex text-left">
+      <img src="../assets/controller.svg" alt="Correct" class="mr-6 h-8 w-8 align-middle " />
       <p>
         Total Juegos Jugados: {$store.gamesPlayed}
       </p>
     </div>
-    <div class="flex m-8 text-left ">
-      <img src="../assets/check.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+    <div class="m-8 flex text-left ">
+      <img src="../assets/check.svg" alt="Correct" class="mr-6 h-8 w-8 align-middle " />
       <p>
-        Total Preguntas Correctas: {$store.correctAnswersCount}
+        Total Preguntas Correctas: {$store.totalQuestionsAnsweredRight}
       </p>
     </div>
-    <div class="flex m-8 text-left">
-      <img src="../assets/questions.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+    <div class="m-8 flex text-left">
+      <img src="../assets/questions.svg" alt="Correct" class="mr-6 h-8 w-8 align-middle " />
       <p>
         Total Preguntas Respondidas: {$store.totalQuestionsAnswered}
       </p>
     </div>
-    <div class="flex m-8 text-left">
-      <img src="../assets/crown-2.svg" alt="Correct" class="w-8 h-8 mr-6 align-middle " />
+    <div class="m-8 flex text-left">
+      <img src="../assets/crown-2.svg" alt="Correct" class="mr-6 h-8 w-8 align-middle " />
       <p>
         Mayor Cantidad de Aciertos: {$store.maxCorrectAnswers}
       </p>
